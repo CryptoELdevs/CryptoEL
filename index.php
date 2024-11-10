1ce8f6568177ce337c7707803bfadc3a8b1578e3
Glicy <Ludovic2008@outlook.com>
2024-11-10 18:14:15 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!-- Cryptography Evaluation Lab -->
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="./js/index.js" defer></script>
+    <title>CryptoEL</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <img src="./assets/img/logoComplet.png" alt="Logo">
+        </div>
+
+        <div class="searchBarContainer">
+            <div class="searchBar">
+                <div class="searchLogoSide">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <input type="text" placeholder="Type your request here...">
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Accueil</a>
+                </li>
+                <li>
+                    <a href="">About us</a>
+                </li>
+                <li>
+                    <a href="">Stay in touch</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
