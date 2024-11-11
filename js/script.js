df4280d1e28ca98e4d62add9a85113091fc883d1
Glicy <158154670+Glicydev@users.noreply.github.com>
2024-11-11 11:41:16 +0100

    remove fetch
    js\script.js

--- js/script.js
+++ js/script.js
@@ -8,29 +8,10 @@ searchbar.addEventListener("click", () => {
     searchInput.focus()
 })
 
-// Partie dédiée à la recherche via api
-let result = null;
-
-searchInput.addEventListener('keydown', async (event) => {
-    if (event.key === 'Enter') {
-        try {
-            const userInput = searchInput.value;
-            const response = await fetch("http://localhost:8000/util/search.php", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: `input=${encodeURIComponent(userInput)}`,
-            });
-
-            if (!response.ok) {
-                throw new Error("Request failed");
-            }
-
-            result = await response.text();
-        } catch (e) {
-            console.error("Error:", e);
-        }
-        console.log(result);
-    }
-});
+// Get cryptos infos part
+const cryptosToFind = [
+    "BTC",
+    "ETH",
+    "LTC"
+];
+const socketToApi = new WebSocket('wss://ws.coinapi.io/v1/');
