3769923204a277c77b1f475a43d3472d039bb471
Glicy <158154670+Glicydev@users.noreply.github.com>
2024-11-14 10:53:57 +0100

    base of cards
    base of cards

--- js/card.js
+++ js/card.js
@@ -1,3 +1,12 @@
+const cards = document.querySelector(".cards")
+
+const cryptos = [
+    "ETH",
+    "BTC",
+    "DOGE",
+    "BNB"
+]
+
 async function getAPIKey() {
     let infos = await fetch("../config/config.json")
     infos = await infos.json()
@@ -23,4 +32,67 @@ async function getNameAndPrice(crypto) {
     console.log(await response.json())
 }
 
-getNameAndPrice("ETH")
+function buildAllCryptoCards() {
+    cryptos.forEach(crypto => {
+        buildCryptoCard(crypto)
+    })
+}
+
+function buildCryptoCard(crypto) {
+        const card = document.createElement("article")
+        card.classList.add("cryptoCard")
+
+        cards.appendChild(card)
+        
+        // Names div part
+        const names = document.createElement("div")
+        names.classList.add("names")
+
+        card.appendChild(names)
+
+        const surname = document.createElement("p")
+        surname.classList.add("surname")
+        surname.textContent = crypto
+
+        names.appendChild(surname)
+
+        const separator = document.createElement("div")
+        separator.classList.add("separator")
+
+        names.appendChild(separator)
+
+        const name = document.createElement("p")
+        name.classList.add("name")
+        name.textContent = "Bitcoin"
+
+        names.appendChild(name)
+
+        // Blochain part
+        const blockchain = document.createElement("p")
+        blockchain.classList.add("blockchain")
+        blockchain.textContent = "Bitcoin"
+
+        card.appendChild(blockchain)
+
+        // Percents part
+        const percents = document.createElement("p")
+        percents.classList.add("percents")
+        percents.textContent = "+4.04%"
+
+        card.appendChild(percents)
+
+        // Price part
+        const price = document.createElement("p")
+        price.classList.add("price")
+        price.textContent = "43825.43$"
+
+        card.appendChild(price)
+
+        // See more button part
+        const seeMore = document.createElement("button")
+        seeMore.textContent = "See more"
+
+        card.appendChild(seeMore)
+}
+
+buildAllCryptoCards()
